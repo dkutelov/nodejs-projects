7a5976c81640a5e4a7c2fdb2b1dc6a6af8cb0c54
dkutelov <dkutelov@digitals.bg>
2022-01-07 19:41:24 +0200

    Launch request tests

---
+++ ztm-NASA-project/server/src/routes/launches/launches.test.js
@@ -0,0 +1,67 @@
+const request = require("supertest");
+const app = require("../../app");
+
+describe("Test GET /launches", () => {
+  test("It should response with 200 success", async () => {
+    const response = await request(app)
+      .get("/launches")
+      .expect("Content-Type", /json/)
+      .expect(200);
+    //expect(response.statusCode).toBe(200);
+  });
+});
+
+describe("Test POST /launches", () => {
+  const completeLaunchData = {
+    mission: "USS Enterprise",
+    rocket: "NCC 1701-D",
+    target: "Kepler 433-1c",
+    launchDate: "January 4, 2028"
+  };
+  const launchDataWithoutDate = {
+    mission: "USS Enterprise",
+    rocket: "NCC 1701-D",
+    target: "Kepler 433-1c"
+  };
+
+  test("It should response with 201 Launch created", async () => {
+    const response = await request(app)
+      .post("/launches")
+      .send(completeLaunchData)
+      .expect("Content-Type", /json/)
+      .expect(201);
+
+    //Testing date
+    const requestDate = new Date(completeLaunchData.launchDate).valueOf();
+    const responseDate = new Date(response.body.launchDate).valueOf();
+    expect(requestDate).toBe(responseDate);
+
+    expect(response.body).toMatchObject(launchDataWithoutDate);
+  });
+
+  test("It should catch missing required property", async () => {
+    const response = await request(app)
+      .post("/launches")
+      .send(launchDataWithoutDate)
+      .expect(400);
+
+    expect(response.body).toStrictEqual({
+      error: "Missing required launch data!"
+    });
+  });
+
+  test("It should catch wrongly formatted date", async () => {
+    const launchDataWithInvalidDate = {
+      mission: "USS Enterprise",
+      rocket: "NCC 1701-D",
+      target: "Kepler 433-1c",
+      launchDate: "Hello"
+    };
+    const response = await request(app)
+      .post("/launches")
+      .send(launchDataWithInvalidDate)
+      .expect(400);
+
+    expect(response.body).toStrictEqual({ error: "Invlid launch date!" });
+  });
+});
