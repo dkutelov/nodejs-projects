d90967b779491f20039bdbd9de7247d4706a1c60
dkutelov <dkutelov@digitals.bg>
2022-01-08 18:48:53 +0200

    Connection to MongoDB Atlas

--- ztm-NASA-project/server/src/server.js
+++ ztm-NASA-project/server/src/server.js
@@ -1,13 +1,26 @@
 const http = require("http");
-const { start } = require("repl");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const PORT = process.env.PORT || 8000;
+
 const app = require("./app");
 const { loadPlanetsData } = require("./models/planets.model");
+
 const server = http.createServer(app);
 
+mongoose.connection.once("open", () => {
+  console.log("MongoDB connection ready!");
+});
+
+mongoose.connection.on("error", (error) => {
+  console.log(error);
+});
+
 async function startServer() {
+  await mongoose.connect(process.env.MONGO_URI);
   await loadPlanetsData();
+
   server.listen(PORT, () => {
     console.log(`🚀 Listening on port: ${PORT} ...`);
   });
