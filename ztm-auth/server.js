7c1b77c41888eeaf0e919062fcb35a5dbc7a704e
dkutelov <dkutelov@digitals.bg>
2022-01-14 09:43:40 +0200

    ZTM Auth: passport google auth process

--- ztm-auth/server.js
+++ ztm-auth/server.js
@@ -1,19 +1,39 @@
 const fs = require("fs");
 const path = require("path");
 const https = require("https");
+
 const helmet = require("helmet");
 const express = require("express");
+const password = require("passport");
+const passport = require("passport");
+const { Strategy } = require("passport-google-oauth20");
 require("dotenv").config();
 
 const PORT = 3000;
+
 const config = {
   CLIENT_ID: process.env.CLIENT_ID,
   CLIENT_SECRET: process.env.CLIENT_SECRET
 };
 
+const AUTH_OPTIONS = {
+  callbackURL: "/auth/google/callback",
+  clientID: config.CLIENT_ID,
+  clientSecret: config.CLIENT_SECRET
+};
+
+function verifyCallback(accessToken, refreshToken, profile, done) {
+  console.log("Google profile: ", profile);
+  // save user in database
+  done(null, profile); // passing profile - passport knows user is authinticated
+}
+
+passport.use(new Strategy(AUTH_OPTIONS, verifyCallback));
+
 const app = express();
 
 app.use(helmet());
+app.use(passport.initialize());
 
 function checkLoggedIn(req, res, next) {
   const isLoggedIn = true; //TODO:
@@ -25,9 +45,24 @@ function checkLoggedIn(req, res, next) {
   next();
 }
 
-app.get("/auth/google", (req, res) => {});
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["email", "profile"]
+  })
+);
 
-app.get("/auth/google/callback", (req, res) => {});
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google", {
+    failureRedirect: "/failure",
+    successRedirect: "/",
+    session: false
+  }),
+  (req, res) => {
+    console.log("Google called us baack!");
+  }
+);
 
 app.get("/auth/logout", (req, res) => {});
 
@@ -37,6 +72,10 @@ app.get("/secret", checkLoggedIn, (req, res) => {
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.get("/failure", (req, res) => {
+  return res.send("Failed to login");
 });
 
 https
